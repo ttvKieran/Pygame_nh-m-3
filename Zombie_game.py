805ba710f84e6269999960fd2def1057c75cb640
haiphong-0132 <bangbangonline1@gmail.com>
2023-11-20 01:17:48 +0700

    Update Pathfinding and something like that

--- Zombie_game.py
+++ Zombie_game.py
@@ -1,5 +1,9 @@
 import pygame
-import pathfinding
+import Finding
+from Finding import matrix1
+
+pathfinder=Finding.Pathfinder(matrix1)
+
 pygame.init()
 SCREEN_SIZE=(1400,787)
 window=pygame.display.set_mode(SCREEN_SIZE)
@@ -21,7 +25,6 @@ class Object:
         self.width=width
         self.height=height
         self.image=image
-
         self.collider=[width,height]
 
         self.velocity=pygame.math.Vector2(0,0)
@@ -54,7 +57,9 @@ class Entity(Object):
         self.frame=0
         self.frames=[1,0,1,2]
         self.frame_timer=0
-        
+        self.current_image=self.tileset[0][0]
+        self.collision_rects = []
+
     def change_direction(self):
         if self.velocity[0]<0:
             self.direction=LEFT
@@ -67,11 +72,11 @@ class Entity(Object):
 
 
     def draw(self):
-        image=pygame.transform.scale(self.tileset[self.frames[self.frame]][self.direction],(self.width,self.height))
-        self.image_rect=image.get_rect()
+        self.current_image=pygame.transform.scale(self.tileset[self.frames[self.frame]][self.direction],(self.width,self.height))
+        self.image_rect=self.current_image.get_rect(center=(self.x,self.y))
         self.change_direction()
-        window.blit(image,(self.x,self.y))
-        pygame.draw.rect(window,("Blue"),(self.x,self.y,self.width,self.height),2)
+        window.blit(self.current_image,self.image_rect)
+        pygame.draw.rect(window,("Blue"),self.image_rect,2)
 
         # self.tileset[self.frames[self.frame][self.direction]][1]=(self.x,self.y,self.width,self.height)
         if self.velocity[0]==0 and self.velocity[1]==0:
@@ -91,12 +96,14 @@ class Entity(Object):
     def update(self):
         self.x+=self.velocity[0]*self.speed
         self.y+=self.velocity[1]*self.speed
+        self.image_rect=self.current_image.get_rect(center=(self.x,self.y))
+
         # self.tileset[self.frames[self.frame][self.direction]][1]=(self.x,self.y,self.width,self.height)
         self.draw()
 
 
-def load_tileset(path,width,height):
-    image=pygame.image.load(path).convert_alpha()
+def load_tileset(filename,width,height):
+    image=pygame.image.load(filename).convert_alpha()
     image_width, image_height=image.get_size()
     tileset=[]
     for tile_x in range(0,image_width//width):
@@ -108,51 +115,71 @@ def load_tileset(path,width,height):
     
     return tileset
 
-def check_collisions(obj1, obj2):
-    x1,y1 =obj1.get_center()
-    x2,y2 =obj2.get_center()
-    w1,h1 =obj1.collider[0]/2, obj1.collider[1]/2
-    w2,h2 =obj2.collider[0]/2, obj2.collider[1]/2
-    if x1+w1>x2-w2 and x1-w1<x2+w2:
-        return y1+h1>y2-h2 and y1-h1<y2+h2
-    return False
 
 class Enemy(Entity):
     def __init__(self,x,y,width,height,tileset,speed,health):
         super().__init__(x,y,width,height,tileset,speed,health)
 
-        # self.max_width=width
-        # self.max_height=height
-
-        # self.width=0
-        # self.height=0
-
         self.collider=[width,height]
 
-        
-        # self.start_timer=pygame.time.get_ticks()
-    
-    # def cooldown(self,time):
-    #     a=pygame.time.get_ticks()-self.start_timer
-    #     if a<time*1000:
-    #         self.x=-1
-    #         self.y=-1
-        
-        # self.width = int(self.max_width*(a/time*1000))
-        # self.height = int(self.max_height*(a/time*1000))
         enemies.append(self)
-    def update(self,player):
-        enemy_center=enemy.get_center()
-        player_center=player.get_center()
 
-        self.velocity=pygame.math.Vector2(player_center[0]-enemy_center[0], player_center[1]-enemy_center[1])
-        self.velocity.normalize_ip()
+   
 
-        # self.cooldown(2)
-        
-        super().update()
+    
+    def set_path(self,path):
+        self.path=path
+        self.create_collision_rects()
+        self.get_velocity()
 
+    def update(self):
+        # enemy_center=self.get_center()
+        # player_center=player.get_center()
 
+        # self.velocity=pygame.math.Vector2(player_center[0]-enemy_center[0], player_center[1]-enemy_center[1])
+        # self.velocity.normalize_ip()
+        # super().update()
+        pathfinder.create_path(player,self)
+        self.set_path(pathfinder.get_path())
+        self.x +=self.velocity[0]*self.speed
+        self.y +=self.velocity[1]*self.speed
+        self.check_collisions()
+        self.image_rect=self.current_image.get_rect(center=(self.x,self.y))
+
+        super().draw()
+
+    def create_collision_rects(self):
+        if self.path:
+            self.collision_rects = []
+            for point in self.path:
+                x=(point.x*49)+24.5
+                y=(point.y*49)+24.5
+                rect=pygame.Rect((x-2,y-2),(4,4))
+                self.collision_rects.append(rect)
+
+    def check_collisions(self):
+        if self.collision_rects:
+            for rect in self.collision_rects:
+                if rect.collidepoint(self.x,self.y):
+                    del self.collision_rects[0]
+                    self.get_velocity()
+
+    def get_velocity(self):
+        if self.collision_rects:
+            start=pygame.math.Vector2(self.x,self.y)
+            end=pygame.math.Vector2(self.collision_rects[0].center)
+            self.velocity = (end-start).normalize()
+        else:
+            self.velocity = pygame.math.Vector2(0,0)
+            self.path=[]
+
+    def change_direction(self):
+        super().change_direction()
+
+        if self.velocity[1]>self.velocity[0]>0:
+            self.direction=DOWN
+        elif self.velocity[1]<self.velocity[0]<0:
+            self.direction=UP
     def take_damage(self,damage):
         self.health-=damage
         if self.health<=0:
@@ -160,15 +187,17 @@ class Enemy(Entity):
 
 
     def destroy(self):
-        spawn_particles(self.x,self.y)
+        spawn_particles(self.x-self.width/2,self.y-self.height/2)
+        # window.blit(pygame.image.load("Zombies/particles.png"),(self.x,self.y))
         objects.remove(self)
         enemies.remove(self)    
+
     def get_center(self):
         return self.x+self.width/2,self.y+self.height/2
         
 
 def spawn_particles(x,y):
-    particle=Object(x,y,75,75,pygame.image.load("Zombie/Zombies/particles.png"))
+    particle=Object(x,y,50,50,pygame.image.load("Zombies/Slime.png"))
     particles.append(particle)
 
 class Player:
@@ -178,7 +207,7 @@ class Player:
         self.width=width
         self.height=height
         self.tileset=pygame.image.load(tileset)
-        self.tileset_rect=self.tileset.get_rect()
+        self.tileset_rect=self.tileset.get_rect(center=(x,y))
         self.speed=speed
         self.health=health
         self.velocity=[0,0]
@@ -187,7 +216,9 @@ class Player:
     def update(self):
         self.x+=self.velocity[0]*self.speed
         self.y+=self.velocity[1]*self.speed
-        pygame.draw.rect(window,("Blue"),(self.x,self.y,self.width,self.height))
+        self.tileset_rect=self.tileset.get_rect(center=(self.x,self.y))
+        window.blit(self.tileset,self.tileset_rect)
+        # pygame.draw.rect(window,("Blue"),(self.x,self.y,self.width,self.height))
    
     def get_center(self):
         return self.x+self.width/2,self.y+self.height/2
@@ -205,11 +236,13 @@ def check_input(key,value):
 player_input={"left": False, "right": False, "up":False, "down":False}
 
 
-player=Player(1280/2,780/2,75,75,"D:/PythonE/Zombie/Zombies/Wizard Zombie.png",5,3)
-enemy=Enemy(100,100,100,100,"Zombie/Zombies/Baby Zombie.png",2,3)
-# 
+player=Player(1280/2,780/2,75,75,"Zombies/Wizard Zombie.png",3,3)
+enemy=Enemy(100,100,50,50,"Zombies/Baby Zombie.png",2,3)
+
 Run=True
+
 while Run:
+    window.fill("White")
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             Run=False
@@ -217,22 +250,24 @@ while Run:
             check_input(event.key,True)
         elif event.type==pygame.KEYUP:
             check_input(event.key,False)
+
     for p in particles:
         p.image.set_alpha(p.image.get_alpha()-1)
         if p.image.get_alpha()==0:
             objects.remove(p)
-            particles.remove(p)
+            particles.insert(0,p)
+
+    for i in range (0,len(objects)):
+        objects[i].update()
+   
+    for e in enemies:
+        if pygame.Rect.colliderect(e.image_rect,player.tileset_rect)==True:
+            player.health-=1
+            e.destroy()
             continue
-        objects.remove(p)
-        objects.insert(0,p)
-    if pygame.Rect.colliderect(enemy.image_rect,player.tileset_rect):
-        player.health-=1
-        enemy.destroy()
-        continue
-    window.fill("White")
+
     player.velocity[0]= player_input["right"] - player_input["left"]
     player.velocity[1]= player_input["down"] - player_input["up"]
-    enemy.update(player)
     player.update()
     clock.tick(60)
     pygame.display.update()
